6b9ab3659db3e8ce99f7566933528a029cbfd012
phusomnia <tuanhphu2004thcs@gmail.com>
2025-08-20 15:57:57 +0700

    update

---
+++ WebAPI/Features/Cache/CacheController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using WebAPI.Features.Cache;
+
+[ApiController]
+[Route("/api/v1/")]
+public class CacheController : ControllerBase
+{
+    private readonly RedisConfig _redisConfig;
+    private readonly ICacheBase _redisStorage;
+
+    public CacheController(RedisConfig redisConfig, ICacheBase redisStorage)
+    {
+        _redisConfig = redisConfig;
+        _redisStorage = redisStorage;
+    }
+
+    [HttpGet("cache/test")]
+    public IActionResult test()
+    {
+        return Ok(_redisStorage.test());
+    }
+
+    [HttpGet("cacheConfig")]
+    public IActionResult checkConfig()
+    {
+        Console.WriteLine("Config {0}", _redisConfig.redis().Multiplexer.Configuration);
+        return Ok("Ok");
+    }
+
+    [HttpPost("cache")]
+    public IActionResult setValue(RedisDTO req)
+    {
+        return Ok(_redisStorage.set(req.Key, req.Value));
+    }
+
+    [HttpGet("cache")]
+    public IActionResult getValue(string key)
+    {
+        return Ok(_redisStorage.get(key));
+    }
+}
+
+public class RedisDTO
+{
+    public string Key { get; set; }
+    public object Value { get; set; }
+}
---
+++ WebAPI/Features/Cache/RedisConfig.cs
@@ -0,0 +1,34 @@
+using StackExchange.Redis;
+
+namespace WebAPI.Features.Cache;
+using WebAPI.Annotation;
+using NRedisStack;
+
+[Configuration]
+public class RedisConfig
+{
+    private readonly IConnectionMultiplexer _muxer;
+    private readonly IDatabase _db;
+    private readonly ConfigurationOptions _conf;
+
+    public RedisConfig(
+        IConfiguration config
+    )
+    {
+        String url = config["Redis:url"] ?? "";
+        Int32 port = Int32.Parse(config["Redis:port"]!);
+        String password = config["Redis:password"] ?? "";
+        String user  = config["Redis:user"] ?? "";
+        Console.WriteLine($"Redis URL: {url}, Port: {port}, Password: {password}, User: {user}");
+        _conf = new ConfigurationOptions
+        {
+            EndPoints= { {url, port} },
+            User = user,
+            Password = password,
+        };
+        _muxer = ConnectionMultiplexer.Connect(_conf);
+        _db = _muxer.GetDatabase();
+    }
+
+    public IDatabase redis() => _db;
+}
